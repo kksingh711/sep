c14a9ac9db68fb6a7b50d7e9815967ac726321d6
Kanhaiya Kumar <kanhaiya.kumar2019@vitstudent.ac.in>
2021-05-11 08:22:34 +0530

    add start

--- auth.js
+++ auth.js
@@ -1,44 +1,51 @@
-var express=require("express");
-const passport = require("passport");
-var app=express();
-var bodyPaeser=require("body-parser");
-var user=require("./model/user");
-app.use(express.static("public"));
-app.use(passport.initialize());
-const mongoose=require("mongoose");
+const express = require('express');
+const app=express();
+var passport=require("passport");
+const mongoose = require('mongoose');
+const localstrategy=require("passport-local");
+passportLocalMongoose=require("passport-local-mongoose");
+const User=require("./model/user");
 mongoose.connect("mongodb://localhost:27017/autho", { useNewUrlParser: true,useUnifiedTopology: true });
-app.use(bodyPaeser.urlencoded({extended:true}));
+mongoose.set('useCreateIndex', true);
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(passport.initialize());
 app.use(passport.session());
 app.use(require("express-session")({
-    secret: "Hello this can be anything and is used to encode and decode",
+    secret:"sdarfsvav",
     resave:false,
     saveUninitialized:false
 }));
-passport.serializeUser(user.serializeUser());
-passport.deserializeUser(user.deserializeUser());
-
-
-var topicdb=new mongoose.model("topicdb",topicdbSchema);
+passport.use(new localstrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.get("/",function (req,res) {
-    res.render("signupp.ejs");
+    res.render("indexx.ejs");
 });
-app.post("/",function(req,res){
-    console.log(req.body.username);
-    console.log(req.body.password);
-    user.register(new user({user:req.body.username}),req.body.password,function(err,use) {
-        if(!err){
+app.post("/",function (req,res) {
+    User.register(new User({username:req.body.usern}),req.body.password,function(error,us){
+        if(!error){
             passport.authenticate("local")(req,res,function(){
-                res.redirect("/secret");
-            })
-        }else{
-            console.log("errir");
-            res.render("signupp.ejs");
-        }
-    });
+                return res.redirect("./pitch");
+            })}
+        else{
+            console.log(error);
+            return res.redirect("./pitch");
+        }}
+    );
+   
+});
+app.get("/pitch",function(req,res){
+    res.render("pitch.ejs");
+});
+app.post("/pitch",passport.authenticate("local",{
+    successRedirect:"/secret",
+    failureRedirect:"/pitch"
+}),function(req,res){
 });
 app.get("/secret",function (req,res) {
-    res.render("notfound.ejs");
+    res.render("secret.ejs");
 });
-app.listen(3000,function () {
+app.listen(3000,function (req,res) {
     console.log("Server started");
 });
